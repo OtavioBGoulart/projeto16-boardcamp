bc98ea744501eb6fa4a151ed5efb72137e9df3e6
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-11 19:29:32 -0300

    feat: game router

---
+++ src/routes/games.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getGames, createGame } from "../controllers/games.controller.js"
+import { validateGame } from "../middlewares/gameValidate.middleware.js";
+
+const router = Router();
+
+router.get("/games", getGames);
+router.post("/games", validateGame, createGame)
+
