702fa8fccbff39d26089af37b19ff04636d1025f
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-12-12 12:25:15 -0300

    feat: update costumer controller

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -41,7 +41,7 @@ export async function getCustomerById(req, res) {
         console.log(error);
         res.sendStatus(500);
     }
-      
+
 
 }
 
@@ -60,6 +60,29 @@ export async function createCustomer(req, res) {
         `, [name, phone, cpf, birthday]);
 
         res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+export async function updateCustomer(req, res) {
+    const { id } = req.params;
+    const { name, phone, cpf, birthday } = req.body;
+
+    if (isNaN(parseInt(id))) return res.sendStatus(400);
+
+
+    try {
+        const updateRefuse = await connectionDB.query('SELECT id FROM customers WHERE cpf = $1 AND id != $2;', [cpf, id]);
+        if (updateRefuse.rowCount > 0) return res.sendStatus(409);
+
+        await db.query(`
+        UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 
+        WHERE id = $5;
+    `, [name, phone, cpf, birthday, id])
+
+        res.sendStatus(200);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
